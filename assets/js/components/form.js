5e1d988086bc70d98857e5a6b670cd0b83324a32
Nathan Burkett <nathan@nathanb.me>
2016-10-07 17:06:48 -0700

    Improve UX (#6)
    * Handle styling QA
    
    * Update styling
    
    * Improve UX with js
    
    * Update index.html to follow improved UX
    
    * Increment app.css and app.js
    
    * Add loader
    
    * Improve grid effect on scroll
    
    * Update styling
    
    * Remove <br> from header
    
    * Add 2x broadband logo
    
    * Increment app.css and app.js
    
    * Remove header and footer 800-850 breakpoint layout styling
    
    * Handle scroll event more gracefully. Unbind when finished
    
    * Move interaction--scale class on to grid class
    
    * Increment public assets
    
    * Run grid logo animation sooner

---
+++ assets/js/components/form.js
@@ -0,0 +1,32 @@
+ const formHandling = () => {
+     // Get the text inputting form els
+     const formEls = document.querySelectorAll('.form__input, .form__textarea');
+     
+     // Loop the form els
+     for (let i = 0; i < formEls.length; i++) {
+         // Cache the current element
+         let el = formEls[i];
+         // Track 'input' events on element
+         el.addEventListener('input', (event) => {
+             // Cache the event.target
+             const target = event.target;
+             // If target has a value
+             if (target.value) {
+                 // And if classList doesn't contain active class
+                 if (!target.classList.contains('form__has-value')) {
+                     // Add active class
+                     target.classList.add('form__has-value');
+                 }
+             // Else if target doesn't have a value
+             } else {
+                 // If classList contains active class
+                 if (target.classList.contains('form__has-value')) {
+                     // Remove active class
+                     target.classList.remove('form__has-value');
+                 }
+             }
+         });
+     }
+ }
+// Export so we can use as a module
+export { formHandling };
---
+++ assets/js/components/load.js
@@ -0,0 +1,144 @@
+/**
+ * Fade in on load
+ * @param  {string} load    Class targeting elements to load
+ * @param  {string} loading Class to apply when elements are loading
+ * @param  {string} loaded  Class to apply when elements have loaded
+ * @return {void}
+ */
+const fadeIn = (load, loading, loaded) => {
+    // Get the targeted elements
+    const targets = document.querySelectorAll(`.${load}`);
+    // Get the loader
+    const loader = document.querySelector('.media__loader--holder');
+    // Get the last index
+    const last = targets.length - 1;
+    // If we have targets
+    if (targets.length) {
+        // Loop through targets
+        targets.forEach(target => {
+            // Set the opacity of each element to 0
+            target.style.opacity = 0;
+        });
+        // Watch for window load
+        window.addEventListener('load', () => {
+            // Item index
+            let i = 0;
+            // Loop through targets
+            targets.forEach(target => {
+                // Set timeout based upon index
+                setTimeout(() => {
+                    // Add the loading class
+                    target.classList.add(loading);
+                    // Remove style attribute from element
+                    target.removeAttribute('style');
+                    // Add the loaded class
+                    target.classList.add(loaded);
+                    // After half a second, remove all classes
+                    setTimeout(() => {
+                        target.classList.remove(load,loading,loaded);
+                    }, 500);
+                    // If we're on the last item
+                    if (i === last) {
+                        // Hide the loader
+                        loader.style.opacity = 0;
+                        setTimeout(() => loader.parentNode.removeChild(loader), 400);
+                    }
+                    i++;
+                }, i * 200);
+                // Increate the index
+            });
+        });
+    }
+}
+
+/**
+ * Handle window resizes
+ * @param  {number} bp
+ * @return {void}
+ */
+const onWindowResize = bp => {
+    // Get the video
+    const video = document.querySelectorAll('video')[0];
+    // Set paused to false
+    let paused = false;
+    // Listen for window resize
+    window.addEventListener('resize', () => {
+        // If window outerWidth is less than bp and not paused
+        if (window.outerWidth < bp && !paused) {
+            // Pause the video
+            video.pause();
+            // Set paused to true
+            paused = true;
+            // Exit
+            return;
+        }
+        // If window outerWidth is greater than bp and paused
+        if (window.outerWidth >= bp && paused) {
+            // Play video
+            video.play();
+            // Set paused to false
+            paused = false;
+        }
+    });
+    // Fire resize event on load
+    window.dispatchEvent(new Event('resize', {bubbles: false, cancelable: false}));
+}
+
+/**
+ * Check if element is in viewport
+ * @param  {Element} element 
+ * @param  {float}   start
+ * @param  {float}   stop
+ * @return {boolean}
+ */
+const inViewport = (element, start) => {
+    // Get element dimensions
+    const rect = element.getBoundingClientRect();
+    // Get the document
+    const html = document.documentElement;
+    // Return boolean
+    return (
+        // If element is inside top sweet spot
+        rect.top <= (window.innerHeight * start || html.clientHeight * start)
+    );
+}
+
+/**
+ * Apply class on scroll
+ * @param  {string} active
+ * @return {void}
+ */
+const applyClassOnScroll = (item, active) => {
+    const scrollEvent = () => {
+        // If is in viewport and doesn't have active class
+        if (inViewport(item, 0.7)) {
+            // Add active class
+            item.classList.add(active);
+            unbindScroll();
+            return;
+        }
+    }
+    
+    const unbindScroll = () => {
+        window.removeEventListener('scroll', scrollEvent);
+    }
+    
+    // Dispatch event on page load
+    setTimeout(() => {
+        // Listen for window scroll event
+        window.addEventListener('scroll', scrollEvent);
+        window.dispatchEvent(new Event('scroll', {bubbles: false, cancelable: false}));
+    }, 500);
+}
+
+/**
+ * Parent func to run all page init scripts
+ * @return {void}
+ */
+const pageInit = () => {
+    fadeIn('interaction--load', 'interaction--loading', 'interaction--loaded');
+    onWindowResize(800)
+    applyClassOnScroll(document.getElementById('grid'), 'interaction--scale');
+}
+// Export so we can use as a module
+export { pageInit };
