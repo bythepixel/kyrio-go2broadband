bedd34f5b82d3d17fe8eaace5490ef87085b4c10
Nathan Burkett <nathan@nathanb.me>
2016-12-07 09:54:01 -0800

    Map Checkbox Values to Bool Values on Form Submit (#16)
    * Fix IE9/10 not supporting pointer-events: none in css
    
    * Move affiliate checkbox group to array'd checkboxes
    
    * Watch for form submits and map checkbox values to bool values
    
    * Increment app.css and app.js
    
    * Update mapCheckboxBoolValues func documentation

--- assets/js/components/form.js
+++ assets/js/components/form.js
@@ -124,5 +124,74 @@
          });
      }
  }
+
+/**
+ * Listen for submit events on forms and change the checkbox values
+ * to true/false values
+ * @return {void}
+ */
+const formCheckboxValReplace = () => {
+    // Get all forms on the page
+    const forms = document.querySelectorAll('form');
+
+    // If there are no forms for the current page
+    if (!forms.length) {
+        // Exit
+        return;
+    }
+
+    // Loop the forms
+    for (let i = 0; i < forms.length; i++) {
+        // Cache the current form
+        let form = forms[i];
+        // Get the form's checkboxes
+        const checkboxes = form.querySelectorAll('input[type="checkbox"]');
+        // If no checkboxes
+        if (!checkboxes.length) {
+            // Exit
+            return;
+        }
+
+        // Add submit listener to form
+        form.addEventListener('submit', (event) => {
+            // Prevent the form from submitting
+            event.preventDefault();
+            // Map the checkboxes to true/false values and re-submit
+            mapCheckboxBoolValues(event.target, checkboxes);
+        });
+    }
+};
+
+/**
+ * Map checkboxes values to bool values
+ * @param  {HTMLElement}        form
+ * @param  {Array<HTMLElement>} checkboxes
+ * @return {void}
+ */
+const mapCheckboxBoolValues = (form, checkboxes) => {
+    // Loop the checkboxes
+    for (let i = 0; i < checkboxes.length; i++) {
+        // Cache the current checkbox
+        let checkbox = checkboxes[i];
+
+        // If the checkbox is checked
+        if (checkbox.checked) {
+            // Change the value to true
+            checkbox.value = true;
+        // If the checkbox is not checked
+        } else {
+            // **
+            // We have to set the checked value to true here. Browser default
+            // behavior is to ignore unchecked checkboxes. In order to submit an
+            // unchecked checkbox with a value of false, we have to set it to
+            // checked and attach the value to it.
+            // *
+            checkbox.checked = true;
+            checkbox.value = false;
+        }
+    }
+    // Submit the form normally
+    form.submit();
+};
 // Export so we can use as a module
-export { formHandling, formRevealOnFill };
+export { formHandling, formRevealOnFill, formCheckboxValReplace };
